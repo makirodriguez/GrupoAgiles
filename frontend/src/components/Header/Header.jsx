f573f5ec460d5671a82c50c2be523b6ada733f2b
Octavio <octaviomazzeo@gmail.com>
2021-10-06 10:54:59 -0300

    Add prode section, also Material UI and Axios installation to consume API endpoits

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,11 +1,30 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <div className="p-5 bg-primary d-flex w-100">
-      <div>Logo</div>
-      <div className=" d-flex">
-        <span>Link 1</span>
-        <span>Link 2</span>
-        <span>Link 3</span>
+      <div className="text-white">Logo</div>
+      <div style={{ marginLeft: "auto" }}>
+        <span className="mx-3">
+          <Link to="/" className="text-white text-decoration-none">
+            Home
+          </Link>
+        </span>
+        <span className="mx-3">
+          <Link to="/prode" className="text-white text-decoration-none">
+            Prode
+          </Link>
+        </span>
+        <span className="mx-3">
+          <Link to="/ranking" className="text-white text-decoration-none">
+            Ranking
+          </Link>
+        </span>
+        <span className="mx-3">
+          <Link to="/" className="text-white text-decoration-none">
+            Mi perfil
+          </Link>
+        </span>
       </div>
     </div>
   );
--- frontend/src/pages/Ranking/Ranking.jsx
+++ frontend/src/pages/Ranking/Ranking.jsx
@@ -1,11 +1,54 @@
-const Ranking = () => {
-    console.log("hola");
-    return (
-      <div>
-        <span>Ranking</span>
-      </div>
-    );
-  };
-  
-  export default Ranking;
-  
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+export default function Ranking() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Lista de jugadores</TableCell>
+            <TableCell align="right">Ganados</TableCell>
+            <TableCell align="right">Empatados</TableCell>
+            <TableCell align="right">Perdidos</TableCell>
+            <TableCell align="right">Puntos</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
