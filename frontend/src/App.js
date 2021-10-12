c02bdc47e586db2992da73d2dde7fe6413039291
Macarena Rodriguez <maaaki.rodriguez@gmail.com>
2021-10-12 13:09:02 -0300

    Pongo Login como pagina principal al levantar la app y cambio ruta para ver el perfil

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Prode from "./pages/Prode/Prode";
 import Login from "./pages/Login/Login";
 import Partidos from "./pages/Partidos/Partidos";
 import Predicciones from "./pages/Predicciones/Predicciones";
+import Perfil from "./pages/Perfil/Perfil";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
@@ -17,12 +18,13 @@ function App() {
         <Header />
         <div className="p-5">
           <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/login" exact component={Login} />
+            <Route path="/" exact component={Login} />
+            <Route path="/home" exact component={Home} />
             <Route path="/ranking" exact component={Ranking} />
             <Route path="/prode" exact component={Prode} />
             <Route path="/partidos" exact component={Partidos} />
             <Route path="/predicciones" exact component={Predicciones} />
+            <Route path="/perfil" exact component={Perfil} />
             <Route path="*">
               <Redirect to="/" />
             </Route>
