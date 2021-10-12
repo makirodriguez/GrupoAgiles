0d744cf0dbbc309ac10a971e6f8b4c62c7895f2f
Macarena Rodriguez <maaaki.rodriguez@gmail.com>
2021-10-12 14:17:58 -0300

    Agrego pagina del perfil

---
+++ frontend/src/pages/Perfil/Perfil.jsx
@@ -0,0 +1,11 @@
+const miPerfil = () => {
+    console.log("hola");
+    return (
+      <div className="">
+        <span>Mi perfil</span>
+      </div>
+    );
+  };
+  
+  export default miPerfil;
+  
