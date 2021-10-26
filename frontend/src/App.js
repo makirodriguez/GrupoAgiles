22026e31630d07be7388426e3411d091f63cc4b3
Macarena Rodriguez <maaaki.rodriguez@gmail.com>
2021-10-26 19:21:28 -0300

    Elimino la pestaña de partidos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,11 @@ import Home from "./pages/Home/Home";
 import Ranking from "./pages/Ranking/Ranking";
 import Prode from "./pages/Prode/Prode";
 import Login from "./pages/Login/Login";
-import Partidos from "./pages/Partidos/Partidos";
 import Predicciones from "./pages/Predicciones/Predicciones";
 import Perfil from "./pages/Perfil/Perfil";
 import Torneos from "./pages/Torneos/Torneos";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useEffect } from "react";
-import axios from "axios";
+
 
 function App() {
   
@@ -25,7 +23,6 @@ function App() {
             <Route path="/home" exact component={Home} />
             <Route path="/ranking" exact component={Ranking} />
             <Route path="/prode" exact component={Prode} />
-            <Route path="/partidos" exact component={Partidos} />
             <Route path="/predicciones" exact component={Predicciones} />
             <Route path="/torneos" exact component={Torneos} />
             <Route path="/perfil" exact component={Perfil} />
