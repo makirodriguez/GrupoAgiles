7b02fd2a4462db9565cb8edf819453c3ec296b8d
Macarena Rodriguez <maaaki.rodriguez@gmail.com>
2021-10-27 19:52:38 -0300

    Realizo el filtro para buscar por torneo o usuario

--- frontend/src/pages/Torneos/Torneos.jsx
+++ frontend/src/pages/Torneos/Torneos.jsx
@@ -2,23 +2,105 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import * as React from 'react';
 
+
 export default function Torneos() {
-    const [torneosDelUser, getTorneos] = useState(0);
+    const [torneosDelUser, getTorneosUser] = useState(0);
+    const [user, getUsuarios] = useState (0);
     const [participantes, getParticipantes] = useState(0);
     const [datos, setDatos] = useState({
         nombre: ''
     })
+    const [torneos, getTorneos] = useState(0);
+    const [busqueda, setBusqueda]= useState(null);
+    var [array, setArrayBusqueda] = useState([]);
+    var [array2, setArrayBusqueda2] = useState([]);
+
+    const arrayBusqueda = [];
+   
+    
     useEffect(() => {
         getData();
+        getUsers();
+        getTorneo();
+       
     },[]);
+    
     function getData(){
         axios.get(`http://127.0.0.1:3001/api/torneos`)
         .then((response) => {
             const data = response.data.filter((item => item.UsuarioCreador === Number(localStorage.userID)));
+            getTorneosUser(data)
+        })
+        .catch(() => console.log("error"));
+    }
+    function getUsers(){
+        axios.get(`http://127.0.0.1:3001/api/usuarios`)
+        .then((response) => {
+            const data = response.data
+            getUsuarios(data)
+        })
+        .catch(() => console.log("error"));
+    }
+    function getTorneo(){
+        axios.get(`http://127.0.0.1:3001/api/torneos`)
+        .then((response) => {
+            const data = response.data;
             getTorneos(data)
         })
         .catch(() => console.log("error"));
     }
+    
+        for(let i = 0; i<torneos.length; i++){
+            arrayBusqueda.push(torneos[i].Nombre);
+        }
+        for(let i = 0; i<user.length; i++){
+            arrayBusqueda.push(user[i].Nombre);
+        }
+
+        
+    const filtrar=(terminoBusqueda)=>{
+         var resultadosBusqueda=user.filter((elemento)=>{
+          if((elemento.Nombre.toString().toLowerCase()).includes(terminoBusqueda.toLowerCase())){
+            return elemento.Nombre;
+          }
+        });
+
+        var resultadosBusqueda2=torneos.filter((elemento)=>{
+            if((elemento.Nombre.toString().toLowerCase()).includes(terminoBusqueda.toLowerCase())){
+              return elemento.Nombre;
+            }
+        });
+        //getUsuarios(resultadosBusqueda);
+        //return (<p>{resultadosBusqueda}</p>);
+        var array=resultadosBusqueda.find(e => {
+            if(e.Nombre.toLowerCase() === terminoBusqueda.toLowerCase()){
+                return e.Nombre;
+            }
+        });
+        var array2=resultadosBusqueda2.find(e => {
+            if(e.Nombre.toLowerCase() === terminoBusqueda.toLowerCase()){
+                return e.Nombre;
+            }
+        }); 
+
+        setArrayBusqueda(array);
+        setArrayBusqueda2(array2);
+      }
+
+      console.log('Usuarios:', array)
+      console.log('Torneos:', array2)
+
+
+
+     
+
+      const handleChange= (e) =>{
+        setBusqueda(e.target.value);
+        filtrar(e.target.value);
+      }
+
+   
+
     if (torneosDelUser.length === 1){
         axios.get(`http://127.0.0.1:3001/api/participantesxtorneo/${torneosDelUser[0].TorneoID}`)
         .then((response) => {
@@ -38,6 +120,18 @@ export default function Torneos() {
             </div>
             </div>
             <span className="h5">Hacete premium para poder crear mas torneos!</span>
+            <div class="container">
+                <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                    <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                        <input
+                            className="form-control inputBuscar"
+                            value={busqueda}
+                            placeholder="Búsqueda por Usurio o Torneo"
+                            onChange={handleChange}
+                        />
+                    </form>
+                </div>
+            </div>
             </div>
           );
         } else {
@@ -66,13 +160,14 @@ export default function Torneos() {
                     .then((res) => {
 
                     })
-                    .catch(() => console.log("error"));  
+                    .catch(() => console.log("error")); 
                     window.alert(
                     `Creado con exito`
                   );
                 })
                 .catch(() => console.log("error"));
             }
+
         return (
             <div className="">
               <div className="card d-flex flex-column">
@@ -94,7 +189,19 @@ export default function Torneos() {
              </div>
                 </form>
              </div>
-              
+             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            
+            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                <input
+                    className="form-control inputBuscar"
+                    value={busqueda}
+                    placeholder="Búsqueda por Usuario o Torneo"
+                    onChange={handleChange}    
+                />
+            </form>
+            <p>Resultados de la busqueda: </p>
+        
+            </div>
             </div>
           );
     }
